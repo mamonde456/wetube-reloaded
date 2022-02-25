3896a7693b8da5a74d42022aaf31016d378be486
mamonde456 <mamonde456@gmail..com>
2022-02-25 16:45:51 +0900

    2022-02-25

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,34 @@
-export const join = (req, res) => res.send("Join");
-export const edit = (req, res) => res.send("Dafs");
+import User from "../models/User";
+
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+export const postJoin = async (req, res) => {
+  const { name, username, email, password, password2, location } = req.body;
+  const pageTitle = "Join";
+  if (password !== password2) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: "Password confirmation does not match.",
+    });
+  }
+
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  if (exists) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: "This username/email is already taken.",
+    });
+  }
+
+  await User.create({
+    name,
+    username,
+    email,
+    password,
+    location,
+  });
+  return res.redirect("/login");
+};
+export const edit = (req, res) => res.send("Edit");
 export const remove = (req, res) => res.send("remove");
 export const login = (req, res) => res.send("login");
 export const logout = (req, res) => res.send("logout");
