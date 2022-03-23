b3e8bd844ab02f13511e753ff7502a0ef57dc42b
mamonde456 <mamonde456@gmail..com>
2022-03-23 18:23:28 +0900

    2022-03-23

--- src/models/Video.js
+++ src/models/Video.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const videoSchema = new mongoose.Schema({
   title: { type: String, required: true, trim: true, maxLength: 80 },
   fileUrl: { type: String, required: true },
-  description: { type: String, required: true, trim: true, minLength: 20 },
+  thumbUrl: { type: String, required: true },
+  description: { type: String, required: true, trim: true, minLength: 2 },
   createdAt: { type: Date, required: true, default: Date.now },
   hashtags: [{ type: String, trim: true }],
   meta: {
@@ -16,6 +17,10 @@ videoSchema.static("formatHashtags", function (hashtags) {
   return hashtags
     .split(",")
     .map((word) => (word.startsWith("#") ? word : `#${word}`));
+});
+
+videoSchema.static("changePathFormula", function (urlPath) {
+  return urlPath.replace(/\\/g, "/");
 });
 
 const Video = mongoose.model("Video", videoSchema);
